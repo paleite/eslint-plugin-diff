e4263fe90c66d4d68ca15c2d6a5e1f44ebecb33e
Patrick Eriksson <paleite@users.noreply.github.com>
2022-08-01 18:26:58 +0200

    Bump coverage

--- jest.config.js
+++ jest.config.js
@@ -7,10 +7,10 @@ module.exports = {
   coveragePathIgnorePatterns: [".test-d.ts"],
   coverageThreshold: {
     global: {
-      branches: 75,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      statements: 97,
+      branches: 84,
+      functions: 100,
+      lines: 97,
     },
   },
 };
