edc77b710dd2e2db9766c9aa2653f4d82eeac4a2
Patrick Eriksson <paleite@users.noreply.github.com>
2022-08-01 20:32:24 +0200

    Handle origin in branch name

--- src/processors.ts
+++ src/processors.ts
@@ -13,8 +13,10 @@ import type { Range } from "./Range";
 if (process.env.CI !== undefined) {
   const branch = process.env.ESLINT_PLUGIN_DIFF_COMMIT ?? guessBranch();
   if (branch !== undefined) {
-    process.env.ESLINT_PLUGIN_DIFF_COMMIT = branch;
-    fetchFromOrigin(branch);
+    const branchWithoutOrigin = branch.replace(/^origin\//, "");
+    const branchWithOrigin = `origin/${branchWithoutOrigin}`;
+    fetchFromOrigin(branchWithoutOrigin);
+    process.env.ESLINT_PLUGIN_DIFF_COMMIT = branchWithOrigin;
   }
 }
 
