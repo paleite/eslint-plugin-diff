b6ab9454d3c12c2f8452e7db2c18aba29700d8ae
Patrick Eriksson <patrick@capchap.se>
2023-11-20 15:36:06 +0100

    Increase coverage

---
+++ src/index-ci.test.ts
@@ -0,0 +1,18 @@
+process.env.ESLINT_PLUGIN_DIFF_COMMIT = "some-branch";
+process.env.CI = "true";
+import * as child_process from "child_process";
+
+jest.mock("child_process");
+const mockedChildProcess = jest.mocked(child_process, { shallow: true });
+mockedChildProcess.execFileSync.mockReturnValue(
+  Buffer.from("line1\nline2\nline3")
+);
+
+import "./index";
+
+describe("CI", () => {
+  it("should diff against origin", () => {
+    expect(process.env.CI).toBeDefined();
+    expect(process.env.ESLINT_PLUGIN_DIFF_COMMIT).toEqual("origin/some-branch");
+  });
+});
